34bc65c07c3844be595e912a78420f64016792a1
Atharva Kulkarni <atharvaak66@gmail.com>
2021-01-19 18:06:48 +0000

    updated work

--- components/sections/work.tsx
+++ components/sections/work.tsx
@@ -74,7 +74,12 @@ export const Project: React.FC<
             {title}
           </div>
           <div className="text-s"></div>
-          <div className={clsx(!featured && "text-sm", "text-gray-500 group-hover:text-teal-400")}>
+          <div
+            className={clsx(
+              !featured && "text-sm",
+              "text-gray-500 group-hover:text-teal-400"
+            )}
+          >
             {preview}
           </div>
         </div>
@@ -95,32 +100,34 @@ export const Work: React.FC<{
   preview?: boolean;
 }> = ({ projects, preview }) => (
   <div className="bg-gray-900">
-  <div className="container my-8">
-    <h1 className="text-4xl font-bold text-teal-300">Work</h1>
-    <div className="text-2xl text-gray-500">Enterprise UI Design • FullStack Development</div>
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 my-4">
-      {projects.slice(0, preview ? 6 : undefined).map((p) => (
-        <Project key={p.id} featured {...p} />
-      ))}
-    </div>
-    {preview && (
-      <div className="md:grid mt-4 grid-cols-2 sm:grid-cols-4 gap-4 ">
-        {projects.slice(6, 9).map((p) => (
-          <Project key={p.id} className="hidden md:flex" {...p} />
-        ))}
-        <div className="flex flex-col items-center justify-center text-center py-4">
-          <div className="text-lg font-bold text-gray-800">
-            Want to see more?
-          </div>
-          <div className="text-gray-600">Explore more of my work</div>
-          <Link href="/work">
-            <a className="mt-3 bg-gray-800 hover:bg-gray-700 text-gray-100 py-1 px-6 rounded-md">
-              View all
-            </a>
-          </Link>
-        </div>
+    <div className="container my-8">
+      <h1 className="text-4xl font-bold text-teal-300">Best Works</h1>
+      <div className="text-2xl text-gray-500">
+        Enterprise UI Design • FullStack Development
       </div>
-    )}
-  </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 my-4">
+        {projects.slice(0, preview ? 6 : undefined).map((p) => (
+          <Project key={p.id} featured {...p} />
+        ))}
+      </div>
+      {preview && (
+        <div className="md:grid mt-4 grid-cols-2 sm:grid-cols-4 gap-4 ">
+          {projects.slice(6, 9).map((p) => (
+            <Project key={p.id} className="hidden md:flex" {...p} />
+          ))}
+          <div className="flex flex-col items-center justify-center text-center py-4">
+            <div className="text-lg font-bold text-gray-800">
+              Want to see more?
+            </div>
+            <div className="text-gray-600">Explore more of my work</div>
+            <Link href="/work">
+              <a className="mt-3 bg-gray-800 hover:bg-gray-700 text-gray-100 py-1 px-6 rounded-md">
+                View all
+              </a>
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
   </div>
 );
